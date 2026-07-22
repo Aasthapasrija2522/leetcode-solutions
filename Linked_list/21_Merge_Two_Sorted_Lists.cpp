b3e60fba0ba8f20da085c2f38d0a1ed5cf7a5f5a
Pranjal Pasrija <aasthapasrija23@gmail.com>
2026-07-22 13:27:02 +0530

    Add LeetCode 21 - Merge Two Sorted Lists

---
+++ Linked_list/21_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        if (list1 == NULL)
+            return list2;
+
+        if (list2 == NULL)
+            return list1;
+
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* tail = dummy;
+
+        while (temp1 != NULL && temp2 != NULL) {
+
+            if (temp1->val < temp2->val) {
+                tail->next = temp1;
+                tail = temp1;
+                temp1 = temp1->next;
+            }
+            else {
+                tail->next = temp2;
+                tail = temp2;
+                temp2 = temp2->next;
+            }
+        }
+
+        // Attach the remaining nodes
+        if (temp1 != NULL)
+            tail->next = temp1;
+
+        if (temp2 != NULL)
+            tail->next = temp2;
+
+        return dummy->next;
+    }
+};
