ba414a97c4c55063712304960664eb0cd41601e0
Pranjal Pasrija <aasthapasrija23@gmail.com>
2026-08-12 07:44:52 +0530

    Add Leetcode 2958

---
+++ sliding window/Length_of_Longest_Subarray_With_at_Most_K_Frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int left=0;
+        int n=nums.size();
+        int maxlen=0;
+        unordered_map<int,int>mp;
+        for(int right=left;right<n;right++){
+            mp[nums[right]]++;
+            while(mp[nums[right]]>k){
+                mp[nums[left]]--;
+                left++;
+
+            }
+            maxlen=max(maxlen,right-left+1);
+
+        }
+        return maxlen;
+    }
+};
