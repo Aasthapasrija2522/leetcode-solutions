fa74f0a1d70d08de1b452b094cb621c5c8d76815
Pranjal Pasrija <aasthapasrija23@gmail.com>
2026-08-23 19:02:08 +0530

    Add Sum Game solution

---
+++ Daily questions/Sum_game.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int half = n / 2;
+
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            if (num[i] == '?')
+                leftQ++;
+            else
+                leftSum += num[i] - '0';
+        }
+
+        for (int i = half; i < n; i++) {
+            if (num[i] == '?')
+                rightQ++;
+            else
+                rightSum += num[i] - '0';
+        }
+
+        // If total '?' is odd, Alice gets one extra move.
+        if ((leftQ + rightQ) % 2 == 1)
+            return true;
+
+        // Difference in number of '?' on both sides.
+        int qDiff = leftQ - rightQ;
+
+        // Difference in already-fixed sums.
+        int sumDiff = leftSum - rightSum;
+
+        // Bob can win only when these exactly cancel out.
+        return 2 * sumDiff != -9 * qDiff;
+    }
+};
